d8076d466f4127af66230d9edda72f88d74dca2e
Ylenia <yleniamighela@gmail.com>
2024-12-21 16:59:04 +0100

    [fix] add exercise gioco dadi

--- ex-gioco dadi/script.js
+++ ex-gioco dadi/script.js
@@ -4,11 +4,31 @@
 
 // Generare un numero randome da 1 a 6 per il giocatore
 
+let playerRandom = Math.floor(Math.random() *6) +1;
+console.log("il tuo numero é", playerRandom);
+
+
 
 // Generare un numero randome da 1 a 6 per il computer
+let pcRandom = Math.floor(Math.random() *6) +1;
+console.log("Il numero del Pc", pcRandom);
 
 
 // SE il punteggio è più alto hai vinto
 
+if (playerRandom > pcRandom){
+    console.log("Hai vinto");
+}
 
-//    ALTRIMENTI SE il punteggio è basso hai perso
+
+//    SE il punteggio è basso hai perso
+
+else if (playerRandom < pcRandom){
+    console.log("Ha vinto il pc"); 
+}
+
+// ALTRIMENTI avete pareggiato
+else {
+    console.log("Avete pareggiato");
+    
+}
