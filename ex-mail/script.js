cda50f5074b74a9e56d6f3494427aa71998c6bda
Ylenia <yleniamighela@gmail.com>
2024-12-21 14:39:16 +0100

    [fix] add logic

--- ex-mail/script.js
+++ ex-mail/script.js
@@ -2,3 +2,13 @@
 // Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for
 
 
+// Creare un lista di email per la festa
+
+
+// Chiedere alll'utente la sua email
+
+// SE l'email è presente all'interno della lista
+    //  OUTPUT
+    // stamperà la sua conferma  alla festa
+
+// ALTRIMENTI verrà scartata
