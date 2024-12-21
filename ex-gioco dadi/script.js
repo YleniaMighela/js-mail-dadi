6d7dccdbc864d8611f27dc54a6ccfdba507464bd
Ylenia <yleniamighela@gmail.com>
2024-12-21 16:45:32 +0100

    [fix] add logic script

--- ex-gioco dadi/script.js
+++ ex-gioco dadi/script.js
@@ -1,2 +1,14 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+
+// Generare un numero randome da 1 a 6 per il giocatore
+
+
+// Generare un numero randome da 1 a 6 per il computer
+
+
+// SE il punteggio è più alto hai vinto
+
+
+//    ALTRIMENTI SE il punteggio è basso hai perso
